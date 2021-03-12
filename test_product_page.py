20b3af5804920d7e6ccecab663d68c4f8b696b7a
Punch-xe <aftar.xe@gmail.com>
2021-03-12 18:14:43 +0300

    changed tests according to section 4.3 step 14 (final edit)

--- test_product_page.py
+++ test_product_page.py
@@ -6,20 +6,11 @@ import pytest
 
 
 @pytest.mark.need_review
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param(
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-                                      "?promo=offer7",
-                                      marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
+@pytest.mark.parametrize('promo', [*range(7),
+                                   pytest.param(7, marks=pytest.mark.xfail),
+                                   *range(8, 10)])
+def test_guest_can_add_product_to_basket(browser, promo):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo}'
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart()
